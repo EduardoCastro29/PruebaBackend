48a6802770d8da8e3eda4919082b2c6aa3df71bd
EduardoCastro29 <kc6331840@gmail.com>
2025-11-06 11:32:50 -0600

    API REST architecture configuration

--- src/main/java/com/lepique/api_rest_echo/ApiRestEchoApplication.java
+++ src/main/java/com/lepique/api_rest_echo/ApiRestEchoApplication.java
@@ -1,5 +1,6 @@
 package com.lepique.api_rest_echo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiRestEchoApplication {
 
     public static void main(String[] args) {
+
+
+        // Esto sirve para cargar las variables del .env del sistema
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach( entry ->
+                System.setProperty(entry.getKey(), entry.getValue() )
+        );
+
         SpringApplication.run(ApiRestEchoApplication.class, args);
     }
 
